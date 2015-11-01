dbbf2878e4835c6c0a9358cf4d1901c2c97b81f4
Neil Gat <neilsantgat@college.harvard.edu>
2015-10-31 18:54:33 -0700

    added var controller

--- home.js
+++ home.js
@@ -1,4 +1,12 @@
 variables = {};
+parameters = {};
+vnets = {};
+pips = {};
+lbs = {};
+nics = {};
+sas = {};
+vms = {};
+vmsses = {};
 
 function commitsDivHtml(which) {
     ret =
@@ -20,7 +28,7 @@ function populateDetails(which) {
 	    "  <div class='col-md-6'>Variable Name: <input id='varName'></input>" +
 	    "  </div>" +
 	    "  <div class='col-md-6'>Variable Type: " +
-	    "    <select>" +
+	    "    <select id='varType'>" +
 	    "      <option value='string'>string</option>" +
 	    "      <option value='securestring'>securestring</option>" +
 	    "    </select>" +
@@ -79,7 +87,62 @@ function populateDetails(which) {
 }
 
 function addBlock(which) {
-    alert('TODO!');
+    switch (which) {
+    case "VAR":
+	name = $('#varName').val();
+	type = $('#varType').val();
+	if (name in variables) {
+	    alert("variable " + name + "already exists! Please edit or remove it below.");
+	    break;
+	}
+	
+	variables[name] = type;
+	break;
+
+    case "PAR":
+	detailsHtml +=
+	    "<div class='row'>" +
+	    "  <div class='col-md-4'>Parameter Name: <input id='varName'></input>" +
+	    "  </div>" +
+	    "  <div class='col-md-4'>Parameter Type: " +
+	    "    <select>" +
+	    "      <option value='string'>string</option>" +
+	    "      <option value='securestring'>securestring</option>" +
+	    "    </select>" +
+	    "  </div>" +
+	    "  <div class='col-md-4'>Parameter Metadata: <input id='parMD'>" +
+	    "  </div>" +
+	    "</div>";
+	break;
+
+    case "VNET":
+	detailsHtml += "VNET";
+	break;
+
+    case "PIP":
+	detailsHtml += "PIP";
+	break;
+
+    case "LB":
+	detailsHtml += "LB";
+	break;
+
+    case "NIC":
+	detailsHtml += "NIC";
+	break;
+
+    case "SA":
+	detailsHtml += "SA";
+	break;
+
+    case "VM":
+	detailsHtml += "VM";
+	break;
+
+    case "VMSS":
+	detailsHtml += "VMSS";
+	break;
+    }
 }
 
 function nixBlock() {
