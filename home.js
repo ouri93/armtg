77bafd920748c536b3ca52dec9b58ef52f36df60
Neil Gat <neilsantgat@college.harvard.edu>
2015-11-01 00:48:24 -0700

    debugging

--- home.js
+++ home.js
@@ -151,10 +151,10 @@ function populateDetails(which) {
 	    "      <option value='Linux'>Linux</option>" +
 	    "      <option value='Windows'>Windows</option>" +
 	    "    </select>" + 
-	    "  </div"> +
+	    "  </div">; /* +
 	    "  <div class='col-md-3'>" +
 	    "    admin username: <input id='admin username'></input>" +
-	    "  </div>"; /* +
+	    "  </div>" +
 	    "  <div class='col-md-3'>" +
 	    "    admin password: <input id='admin password'></input>" +
 	    "  </div>" +
