ca79f87930dec110e79e406a42d54b9296c0e15a
Neil Sant Gat <negat@microsoft.com>
2015-12-30 02:59:13 +0000

    formatting

--- home.js
+++ home.js
@@ -587,7 +587,7 @@ function createResources() {
 
 function generateTemplate() {
     createResources();
-    $('#output').html(JSON.stringify(baseTemplateObject));
+    $('#output').html(JSON.stringify(baseTemplateObject, null, 2));
 }
 
 function deployToAzure() {
