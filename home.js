8e34de0c99e09ee275050975c82d1239efd70694
Neil Sant Gat <negat@microsoft.com>
2015-12-31 03:24:42 +0000

    formatting

--- home.js
+++ home.js
@@ -422,7 +422,7 @@ $(document).ready(function() {
 
 	controlHtml += "<div class='col-md-" + controlButtonWidth.toString() + "'><button class='btn btn-default' onclick='javascript:populateDetails(\"" + blockType + "\");'>+ " + blockType + "</button></div>";
 
-	currentWidthUsed += numControlButtonsPerRow;
+	currentWidthUsed += controlButtonWidth;
 
 	if (currentWidthUsed == properRowWidth) {
 	    controlHtml += "</div>";
