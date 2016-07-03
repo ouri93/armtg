bdb52513cd1031eedd52f7b5e7286dac763f4f1e
Neil Sant Gat <negat@microsoft.com>
2016-07-03 05:49:21 +0000

    fixed push script to add latest home.html file to commit

--- push.sh
+++ push.sh
@@ -1,3 +1,6 @@
 current_commit=$(git log | head --lines=1)
 cp home_template.html home.html
 sed -i "s/CURRENT_GIT_COMMIT/$current_commit/g" home.html
+git add home.html
+git commit -m "push.sh script changed git commit hash in home.html"
+git push #1 #2
